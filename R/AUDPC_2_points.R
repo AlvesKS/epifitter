358ddef5adf46102b25ae9b616c1e4e8bc5e20a6
Adam H. Sparks <adamhsparks@icloud.com>
2025-01-22 19:11:39 +0800

    Match Kaique's style of package writing

---
+++ R/AUDPC_2_points.R
@@ -0,0 +1,7 @@
+AUDPC_2_points <- function(time, y0, yT) {
+  # eqn. 3 from Jeger and Viljanen-Rollinson (2001), rate parameter r
+  r <- (log(yT / (1 - yT)) - log(y0 / (1 - y0))) / time
+
+  # eqn. 2 from Jeger and Viljanen-Rollinson (2001), AUDPC estimation
+  return(time + log(y0 / yT) / r)
+}
